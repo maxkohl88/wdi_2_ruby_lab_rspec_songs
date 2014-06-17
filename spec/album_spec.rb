441a5bcd8fa903454e286098ebe26556b6fe1379
Max Kohl <maxkohl810@gmail.com>
2014-06-17 16:11:37 -0400

    Albums have artits that you can look at alphabetical array unique.

--- spec/album_spec.rb
+++ spec/album_spec.rb
@@ -3,23 +3,31 @@ require_relative '../lib/album'
 describe Album do
   let(:stairway) { Track.new('Stairway to Heaven', ['Led Zeppelin'], 482, 1.0) }
   let(:kashmir) { Track.new('Kahsmir', ['Led Zeppelin'], 508, 2.0)}
-  let(:greatest_hits) { Album.new('Greatest Hits', [stairway, kashmir]) }
+  let(:california) { Track.new('Hotel California', ['The Eagles'], 390, 1.50) }
+  let(:greatest_hits) { Album.new('Greatest Hits', [stairway, kashmir, california]) }
 
   describe 'attributes' do
     it 'has a title and list of tracks' do
       expect(greatest_hits.name).to eq 'Greatest Hits'
-      expect(greatest_hits.tracks).to eq [stairway, kashmir]
+      expect(greatest_hits.tracks).to eq [stairway, kashmir, california]
     end
   end
 
   describe '#duration' do
     it 'has a duration' do
-      expect(greatest_hits.duration).to eq 990
+      expect(greatest_hits.duration).to eq 1380
     end
   end
+
   describe '#price' do
     it 'has a price' do
-      expect(greatest_hits.price).to eq 3.0
+      expect(greatest_hits.price).to eq 4.5
+    end
+  end
+
+  describe '#artists' do
+    it 'has a list of artists without duplicates' do
+      expect(greatest_hits.artists).to eq ['Led Zeppelin', 'The Eagles']
     end
   end
 end
