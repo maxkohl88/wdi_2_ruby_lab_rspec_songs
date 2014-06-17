762c77b9f9fd178ab308971b21dea743dd435e07
Max Kohl <maxkohl810@gmail.com>
2014-06-17 15:21:46 -0400

    reduce the price of a track by the discount amount

--- lib/track.rb
+++ lib/track.rb
@@ -1,6 +1,11 @@
 class Track
-  attr_reader :name, :artists, :duration, :price
+  attr_reader :name, :artists, :duration
+  attr_accessor :price
   def initialize(name, artists, duration, price)
     @name, @artists, @duration, @price = name, artists, duration, price
+  end
+
+  def set_discount(discount_percentage)
+    self.price = price * (1 - discount_percentage)
   end
 end
--- spec/track_spec.rb
+++ spec/track_spec.rb
@@ -1,14 +1,22 @@
 require_relative '../lib/track'
 
 describe Track do
+  let(:track) { Track.new('Stairway to Heaven', ['Led Zeppelin'], 482, 1.0) }
+
+
   describe '#initialize' do
     it 'has a title, list of artists, duration, price' do
-      track = Track.new('Stairway to Heaven', ['Led Zeppelin'], 482, 0.99)
 
       expect(track.name).to eq 'Stairway to Heaven'
       expect(track.artists).to contain_exactly('Led Zeppelin')
       expect(track.duration).to eq 482
-      expect(track.price).to eq 0.99
+      expect(track.price).to eq 1.0
+    end
+  end
+  describe '#set_discount' do
+    it 'discounts the price by the given amount' do
+      track.set_discount(0.2)
+      expect(track.price).to eq 0.8
     end
   end
 end
